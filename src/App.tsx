89a00c5d9eae6eee0eee5b0d1df7b3b8372b6f93
Devessier <baptiste@devessier.fr>
2023-10-18 10:54:22 +0200

    feat: create machine with two task queues

--- src/App.tsx
+++ src/App.tsx
@@ -17,20 +17,31 @@ import {
 import { useActor, useMachine } from "@xstate/react";
 import { machineOneInvoke } from "./machines/machineOneInvoke";
 import { TaskQueueActorRef } from "./machines/taskQueue";
+import { machineTwoInvokes } from "./machines/machineTwoInvokes";
 
-function TaskQueueStatus({ actorRef }: { actorRef: TaskQueueActorRef }) {
+function TaskQueueStatus({
+  actorRef,
+  taskQueueId,
+}: {
+  actorRef: TaskQueueActorRef;
+  taskQueueId: string;
+}) {
   const [state] = useActor(actorRef);
 
   return (
     <VStack borderWidth={1} p="4" alignItems="start">
-      <Text fontWeight="semibold">Task queue #1</Text>
+      <Text fontWeight="semibold">Task queue #{taskQueueId}</Text>
 
-      <Text>State: <Code>{String(state.value)}</Code></Text>
+      <Text>
+        State: <Code>{String(state.value)}</Code>
+      </Text>
 
       <Box>
         <Text>Pending tasks:</Text>
 
-        <Code whiteSpace="pre">{JSON.stringify(state.context.tasks, null, 2)}</Code>
+        <Code whiteSpace="pre">
+          {JSON.stringify(state.context.tasks, null, 2)}
+        </Code>
       </Box>
     </VStack>
   );
@@ -80,16 +91,95 @@ function Panel1() {
       </HStack>
 
       <VStack alignItems="stretch" mt="12">
-        <Heading as="h2" fontWeight="semibold" fontSize="xl" mb="4">Machine's tasks results</Heading>
+        <Heading as="h2" fontWeight="semibold" fontSize="xl" mb="4">
+          Machine's tasks results
+        </Heading>
 
-        <Code whiteSpace="pre">{JSON.stringify(state.context.tasks, null, 2)}</Code>
+        <Code whiteSpace="pre">
+          {JSON.stringify(state.context.tasks, null, 2)}
+        </Code>
       </VStack>
 
       <VStack alignItems="stretch" mt="12">
-        <Heading as="h2" fontWeight="semibold" fontSize="xl" mb="4">Task queues</Heading>
+        <Heading as="h2" fontWeight="semibold" fontSize="xl" mb="4">
+          Task queues
+        </Heading>
 
         <TaskQueueStatus
           actorRef={state.children["Single task queue"] as TaskQueueActorRef}
+          taskQueueId="1"
+        />
+      </VStack>
+    </Box>
+  );
+}
+
+function Panel2() {
+  const [state, send] = useMachine(machineTwoInvokes);
+
+  return (
+    <Box>
+      <Text>
+        Current state of the queue: <Code>{String(state.value)}</Code>
+      </Text>
+
+      <HStack>
+        <Button
+          size="sm"
+          mt="4"
+          onClick={() => {
+            send({
+              type: "Toggle",
+            });
+          }}
+        >
+          Toggle state
+        </Button>
+
+        {state.matches("Open") ? (
+          <Button
+            size="sm"
+            mt="4"
+            onClick={() => {
+              send({
+                type: "Add task",
+                task: {
+                  id: Math.random().toString(),
+                  payload: {
+                    fileContent: "content",
+                  },
+                },
+              });
+            }}
+          >
+            Add task
+          </Button>
+        ) : null}
+      </HStack>
+
+      <VStack alignItems="stretch" mt="12">
+        <Heading as="h2" fontWeight="semibold" fontSize="xl" mb="4">
+          Machine's tasks results
+        </Heading>
+
+        <Code whiteSpace="pre">
+          {JSON.stringify(state.context.tasks, null, 2)}
+        </Code>
+      </VStack>
+
+      <VStack alignItems="stretch" mt="12">
+        <Heading as="h2" fontWeight="semibold" fontSize="xl" mb="4">
+          Task queues
+        </Heading>
+
+        <TaskQueueStatus
+          actorRef={state.children["Task queue 1"] as TaskQueueActorRef}
+          taskQueueId="1"
+        />
+
+        <TaskQueueStatus
+          actorRef={state.children["Task queue 2"] as TaskQueueActorRef}
+          taskQueueId="2"
         />
       </VStack>
     </Box>
@@ -115,7 +205,7 @@ function App() {
                 <Panel1 />
               </TabPanel>
               <TabPanel>
-                <p>two!</p>
+                <Panel2 />
               </TabPanel>
               <TabPanel>
                 <p>three!</p>
---
+++ src/machines/machineTwoInvokes.ts
@@ -0,0 +1,131 @@
+import { assign, createMachine, sendTo } from "xstate";
+import { Task } from "../types";
+import { TaskQueueActorRef, taskQueue } from "./taskQueue";
+
+/** @xstate-layout N4IgpgJg5mDOIC5QBUDuB7ABASQHYDd0BrMAYmQENYjMAHAJ3QGM5YBLXKTAIzCg9gBtAAwBdRKFrp2AFzbpcEkAA9EAJgBsAOgCMAZj3CAnCaMBWAOxm9GgCwAaEAE9EAWjVqAHFq8a1FtWENYTUjTTUAXwjHNCw8QhJyKhoGZlYOLgAzCjYAG0gRcSQQKVl5RWLVBA1DLXMLI1sgowtPI08HZzc1Wx0tC2FhHR1Bo2EzTzMNCyiYjBwCYjJKajpGFlh2TkxYAFcmFkgCsSVStjkFJSqbMy1bMxMNI31hPSsLRxcEVwMLOp0NGYzDoHkY9G1PLMQLEFgkwFoAMK5aSQcjoKBQfKFU7Sc7lK7qV5aaY9AEaTwDYYBT6IfS2f5qHStV7mMzCVpQmHxJZaADytDAuFIAEEIBBMDJktjimcLhVQFUzL0fJ4vGM3hYNDoejSEADhP07OS2bZPB1hJNOfNuSQ+QKhch0ZiwNLJLi5QS9WEtHpbAN2VYdLY7B1dR4+qrbHopuznkMplFoiBcOgIHAlFzFiQcWVLpU3Kbde5bpZWjUo+SNNM7Fa4ln4UiURAc3i8wrEMJdUHbLXYTz+YKWx78wg3n0wiNhvogRZp13fcT7lGg+YdJ5hnpExEgA */
+export const machineTwoInvokes = createMachine(
+  {
+    id: "Two Invoke",
+    tsTypes: {} as import("./machineTwoInvokes.typegen").Typegen0,
+
+    context: {
+      tasks: [],
+    },
+
+    schema: {
+      context: {} as {
+        tasks: {
+          taskId: string;
+          status: "idle" | "pending" | "success" | "failure";
+        }[];
+      },
+      events: {} as
+        | {
+            type: "Add task";
+            task: Task;
+          }
+        | {
+            type: "Toggle";
+          }
+        | { type: "Task processing begins"; taskId: string }
+        | { type: "Task processing failed"; taskId: string }
+        | { type: "Task processing succeeded"; taskId: string },
+    },
+
+    invoke: [
+      {
+        src: "Task queue",
+        id: "Task queue 1",
+      },
+      {
+        src: "Task queue",
+        id: "Task queue 2",
+      },
+    ],
+
+    states: {
+      Closed: {
+        on: {
+          Toggle: "Open",
+        },
+      },
+
+      Open: {
+        on: {
+          "Add task": {
+            actions: [
+              "Forward task to random task queue",
+              "Assign task to context",
+            ],
+          },
+
+          Toggle: "Closed",
+        },
+      },
+    },
+
+    initial: "Closed",
+
+    on: {
+      "Task processing begins": {
+        actions: "Assign new status to task in context",
+      },
+
+      "Task processing failed": {
+        actions: "Assign new status to task in context",
+      },
+
+      "Task processing succeeded": {
+        actions: "Assign new status to task in context",
+      },
+    },
+  },
+  {
+    actions: {
+      "Forward task to random task queue": sendTo(
+        () => {
+          return `Task queue ${randomIntFromInterval(1, 2)}` as unknown as TaskQueueActorRef;
+        },
+        (_context, event) => ({
+          type: "Add task to queue",
+          task: event.task,
+        })
+      ),
+      "Assign task to context": assign({
+        tasks: (context, event) =>
+          context.tasks.concat({
+            taskId: event.task.id,
+            status: "idle",
+          }),
+      }),
+      "Assign new status to task in context": assign({
+        tasks: (context, event) =>
+          context.tasks.map((task) => {
+            if (task.taskId !== event.taskId) {
+              return task;
+            }
+
+            return {
+              taskId: task.taskId,
+              status:
+                event.type === "Task processing begins"
+                  ? "pending"
+                  : event.type === "Task processing succeeded"
+                  ? "success"
+                  : event.type === "Task processing failed"
+                  ? "failure"
+                  : "idle",
+            } as const;
+          }),
+      }),
+    },
+    services: {
+      "Task queue": taskQueue,
+    },
+  }
+);
+
+function randomIntFromInterval(min: number, max: number): number {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
